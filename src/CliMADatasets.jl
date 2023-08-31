652f21405b84ce8caac89cd5bb8eba083b9d60d5
kmdeck <kdeck@caltech.edu>
2023-08-31 10:49:08 -0700

    add correlated ou 1d

--- src/CliMADatasets.jl
+++ src/CliMADatasets.jl
@@ -9,17 +9,20 @@ include("datasets/celeba_hq.jl")
 include("datasets/turbulence_2d.jl")
 include("datasets/turbulence_2d_context.jl")
 include("datasets/correlated_ou_2d.jl")
+include("datasets/correlated_ou_1d.jl")
 
 export CelebAHQ
 export Turbulence2D
 export Turbulence2DContext
 export CorrelatedOU2D
+export CorrelatedOU1D
 
 function __init__()
     __init__celeba_hq()
     __init__turbulence_2d()
     __init__turbulence_2d_context()
     __init__correlated_ou_2d()
+    __init__correlated_ou_1d()
 end
 
 end #module
